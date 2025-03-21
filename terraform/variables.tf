b681f0137772e6ada3d70de9546df9611e314c3d
actuallypav <professional.extras708@passinbox.com>
2025-03-21 16:38:52 +0000

    variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,9 @@
 variable "region" {
   description = "The region within which this tf deployment can be found"
-  type = string
+  type        = string
+}
+
+variable "domain_name" {
+  description = "The name of the domain"
+  type        = string
 }
