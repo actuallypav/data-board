9f33944473db5626e0ecda05bcedef77583217d1
actuallypav <pawel.ambrozy@pm.me>
2025-07-03 05:41:31 +0100

    fmt

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -86,9 +86,9 @@ resource "aws_security_group" "lambda_sg" {
   vpc_id      = aws_vpc.main_data.id
 
   egress {
-    from_port = 3306
-    to_port   = 3306
-    protocol  = "tcp"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
 }
