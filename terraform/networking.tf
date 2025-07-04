534908150a9a20eecbb67633281b0b5ca7c48b20
actuallypav <professional.extras708@passinbox.com>
2025-07-04 08:29:25 +0100

    add allow_metabase sg

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -118,6 +118,35 @@ resource "aws_security_group" "lambda_sg" {
   }
 }
 
+resource "aws_security_group" "allow_metabase" {
+  name        = "allow_metabase"
+  description = "Allow HTTP/HTTPS for Metabase"
+  vpc_id      = aws_vpc.main_data.id
+}
+
+#remove if not needing http
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_metabase.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.allow_metabase.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.allow_metabase.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
 # #ONLY FOR DEBUGqGING
 # resource "aws_vpc_security_group_ingress_rule" "allow_metabas_ui" {
 #   security_group_id = aws_security_group.allow_metabase.id
