3be4ea08cd2b0405cb89f0852e02beaac26c1b1f
actuallypav <professional.extras708@passinbox.com>
2025-07-04 10:09:53 +0100

    typo

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -130,10 +130,10 @@ resource "aws_security_group" "lambda_sg" {
 # }
 
 
-# resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-#   security_group_id = aws_security_group.allow_metabase.id
-#   cidr_ipv4         = "0.0.0.0/0"
-#   from_port         = 22
-#   ip_protocol       = "tcp"
-#   to_port           = 22
-# }
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_metabase.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
